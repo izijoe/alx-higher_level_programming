2c814ee9dca2f1d1175cd3d56f77b2cf9b9c60b1
izijoe <jospaco2001@yahoo.com>
2023-01-05 02:36:06 -0800

    calculator_1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Argvs:
+        a: first integer
+        b: second integer
+
+    Rturns:
+        The return valve. a  + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    """My subtraction function
+
+    Argvs:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return valve. a - b
+    """
+    return (a - b)
+
+def mul(a, b):
+    """My multiplication function
+
+    Argvs:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return valve. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My divition function
+
+    Argvs:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return valve. a / b
+        """
+        return int(a / b)
