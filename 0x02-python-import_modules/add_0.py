fd0a279d8c7bb392dfdb213dab2add5946f05a5c
izijoe <jospaco2001@yahoo.com>
2023-01-05 03:02:12 -0800

    add_0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+        Returns:
+            The return valve. a + b
+        """
+        return (a + b)
