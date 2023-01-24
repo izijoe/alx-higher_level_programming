c1b40ab23294277dd4e6cee8f30a2d8d0c1eb5ef
izijoe <jospaco2001@yahoo.com>
2023-01-24 13:39:57 -0800

    my commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class square."""
+
+
+class square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            """
+            self.__size = size
