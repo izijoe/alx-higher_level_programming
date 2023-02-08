8bf12ad8a063a3befa81517591fb682d4a434974
izijoe <jospaco2001@yahoo.com>
2023-02-08 14:23:05 -0800

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+# 6-from_json_string.py
+"""Defines a JSON-to-object function."""
 import json
 
 
-"""
-from_json_string function module.
-
-Define from_json_string function.
-"""
-
 
 def from_json_string(my_str):
-    """Returns an object made from JSON string.
-    my_str: the string to load from JSON.
-    """
+    """Return the Python object representation of a JSON string."""
     return json.loads(my_str)
