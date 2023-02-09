f3976bf1bcb75f09294a79ff7bb4be17ae2fe54f
izijoe <jospaco2001@yahoo.com>
2023-02-08 16:10:37 -0800

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,12 +21,19 @@ class Student:
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
-        If attrs is a list of string, represents only those attributes included in the list.
+        If attrs is a list of strings, represents only those attributes included in the list.
+
+        Agrs:
+        attrs (list): (Optional) The attributes to represent."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+    
+    def reload_fromjson(self, json):
+        """Replace all attributes of the Student.
 
         Args:
-            attrs (list): (optional) The attributes to represent.
-            """
-            if (type(attrs) == list and
-                    all(type(ele) == str for ele in attrs)):
-                return {k: getattr(self, k) for k in attrs if hasattr(self, k))
-            return self.__dict__
+        json (dict): The key/value pairs to replace attributes with."""
+        for k, v in json.items():
+            setattr(self, k, v)
