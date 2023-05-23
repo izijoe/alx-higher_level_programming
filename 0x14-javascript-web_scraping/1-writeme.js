7797b2ebf54200c79f0b2bfd0e2923b9c57f2824
izijoe <jospaco2001@yahoo.com>
2023-05-23 11:57:13 +0000

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+/* Script writes a string to a file */
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-	if (error) console.log(error);
-});
+
+if (process.argv.length >= 4) {
+  const path = process.argv[2];
+  const string = process.argv[3];
+
+  fs.writeFile(path, string, err => {
+    if (err) { console.log(err); }
+  });
+}
