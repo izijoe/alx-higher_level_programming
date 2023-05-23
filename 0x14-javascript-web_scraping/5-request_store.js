4f89c6bf97b9d16c460f7b43f56cc6f126109ebb
izijoe <jospaco2001@yahoo.com>
2023-05-23 12:00:39 +0000

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
-});
+if (process.argv.length >= 4) {
+  const url = process.argv[2];
+  const path = process.argv[3];
+
+  request(url, (err, response, body) => {
+    if (err == null) {
+      fs.writeFile(path, body, err => {
+        if (err) throw err;
+      });
+    }
+  });
+}
